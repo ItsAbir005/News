c42cc7e2959acc327c47120e8d1a221e6fb0f5d6
ItsAbir005 <abirmaity44@gmail.com>
2025-03-24 23:23:51 +0530

    add

---
+++ script.js
@@ -0,0 +1,31 @@
+async function fetchNews(page) {
+    const apiKey = "2ce9d4509a274e8084beb67dcbea821c";
+    const url = `https://newsapi.org/v2/top-headlines?country=us&page=${page}&apiKey=${apiKey}`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(JSON.stringify(data));
+
+    let contentContainer = document.querySelector(".content");
+    let str = "";
+
+    for (let item of data.articles) {
+        str += `
+            <div class="card my-4 mx-2" style="width: 18rem;">
+                <img src="${item.urlToImage}" class="card-img-top" alt="News Image">
+                <div class="card-body">
+                    <h5 class="card-title">${item.title}</h5>
+                    <p class="card-text">${item.description}</p>
+                    <a href="${item.url}" target="_blank" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+        `;
+    }
+
+    contentContainer.innerHTML = str;
+};
+
+fetchNews();
+
+
+
